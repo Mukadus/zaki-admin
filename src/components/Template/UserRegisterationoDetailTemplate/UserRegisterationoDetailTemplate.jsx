ad0dfa30577dc6e25a5270e838c2110bad42fbeb
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-10-07 19:15:37 +0500

    pro

--- src/components/Template/UserRegisterationoDetailTemplate/UserRegisterationoDetailTemplate.jsx
+++ src/components/Template/UserRegisterationoDetailTemplate/UserRegisterationoDetailTemplate.jsx
@@ -1,13 +1,18 @@
+'use client'
 import ShadowWrapper from '@/components/atoms/ShadowWrapper/ShadowWrapper'
 import TopHeader from '@/components/atoms/TopHeader/TopHeader'
 import TableHeader from '@/components/molecules/TableHeader/TableHeader'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from 'react-bootstrap';
 import classes from "./UserRegisterationoDetailTemplate.module.css"
 import Wrapper from '@/components/atoms/Wrapper/Wrapper';
 import PersonalInfo from '@/components/atoms/PersonalInfo/PersonalInfo';
+import { useSearchParams } from 'next/navigation';
 
 const UserRegisterationoDetailTemplate = ({slug}) => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id') || "";
+
   return (
     <>
         <Container>
