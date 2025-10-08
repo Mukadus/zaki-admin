d8485fbb94c8c7fe47add67954e6079f17807e44
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-10-08 16:26:44 +0500

    AnalyticsTemplate

--- src/components/Template/AnalyticsTemplate/AnalyticsTemplate.jsx
+++ src/components/Template/AnalyticsTemplate/AnalyticsTemplate.jsx
@@ -3,16 +3,54 @@ import classes from "./AnalyticsTemplate.module.css"
 import { Col, Container, Row } from 'react-bootstrap';
 import TopHeader from '@/components/atoms/TopHeader/TopHeader';
 import AnalyticsCard from '@/components/atoms/AnalyticsCard/AnalyticsCard';
+import { HiMiniUsers, HiMiniUserGroup, HiMiniCalendarDays, HiMiniCurrencyDollar } from 'react-icons/hi2';
 
 const AnalyticsTemplate = () => {
+  // Sample analytics data
+  const analyticsData = [
+    {
+      title: "Total Therapists",
+      value: "344",
+      change: "+36%",
+      icon: HiMiniUsers
+    },
+    {
+      title: "Total Clients", 
+      value: "344",
+      change: "+36%",
+      icon: HiMiniUserGroup
+    },
+    {
+      title: "Total Appointments",
+      value: "344", 
+      change: "+36%",
+      icon: HiMiniCalendarDays
+    },
+    {
+      title: "Revenue",
+      value: "$344",
+      change: "+36%", 
+      icon: HiMiniCurrencyDollar
+    }
+  ];
+
   return (
     <div>
       <Container>
         <TopHeader title="Analytics" backButton={false} />
         <Row>
-          <Col lg={6}>  
-          <AnalyticsCard />
-          </Col>
+          {analyticsData.map((data, index) => (
+            <Col key={index} lg={6} md={6} sm={12} className="mb-3">
+              <AnalyticsCard 
+                title={data.title}
+                value={data.value}
+                change={data.change}
+                icon={data.icon}
+              />
+            </Col>
+          ))}
+          <Col md={6}>
+          <p>dedew</p></Col>
         </Row>
       </Container>
     </div>
@@ -20,3 +58,24 @@ const AnalyticsTemplate = () => {
 }
 
 export default AnalyticsTemplate
+
+{/* <Row>
+          <Col  lg={6} md={6} sm={12} className="mb-3">
+          <Row className='gy-4'>
+          {analyticsData.map((data, index) => (
+            <Col lg={6}>
+              <AnalyticsCard 
+                title={data.title}
+                value={data.value}
+                change={data.change}
+                icon={data.icon}
+                key={index}
+                />
+                </Col>
+          ))}
+
+          </Row>
+            </Col>
+          <Col md={6}>
+          <p>dedew</p></Col>
+        </Row> */}
--- src/components/atoms/AnalyticsCard/AnalyticsCard.jsx
+++ src/components/atoms/AnalyticsCard/AnalyticsCard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import classes from "./AnalyticsCard.module.css";
 import { HiMiniUsers } from "react-icons/hi2";
 
-export default function AnalyticsCard() {
+export default function AnalyticsCard({ title, value, change, icon: Icon = HiMiniUsers }) {
   return (
     <div className={classes.card}>
       <div className={classes.cardHeader}>
-        <h5>Total Therapists</h5>
+        <h5>{title}</h5>
         <div className={classes.iconDiv}>
-          <HiMiniUsers className={classes.icon} />
+          <Icon className={classes.icon} />
         </div>
       </div>
       <div className={classes.valuesDiv}>
-        <h3>150</h3>
-        <p>+5%</p>
+        <h3>{value}</h3>
+        <p>{change}</p>
       </div>
     </div>
   );
