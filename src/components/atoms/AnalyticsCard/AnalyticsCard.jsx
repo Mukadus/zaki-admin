234570b0b1ed7414f906543c3baa50eac61570c6
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-10-08 16:10:17 +0500

    AnalyticsCard

--- src/components/atoms/AnalyticsCard/AnalyticsCard.jsx
+++ src/components/atoms/AnalyticsCard/AnalyticsCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import classes from "./AnalyticsCard.module.css"
+import React from "react";
+import classes from "./AnalyticsCard.module.css";
+import { HiMiniUsers } from "react-icons/hi2";
 
 export default function AnalyticsCard() {
   return (
-    <div>
-      
+    <div className={classes.card}>
+      <div className={classes.cardHeader}>
+        <h5>Total Therapists</h5>
+        <div className={classes.iconDiv}>
+          <HiMiniUsers className={classes.icon} />
+        </div>
+      </div>
+      <div className={classes.valuesDiv}>
+        <h3>150</h3>
+        <p>+5%</p>
+      </div>
     </div>
-  )
+  );
 }
