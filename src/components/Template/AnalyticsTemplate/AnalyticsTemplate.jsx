55c2436cdedf570c32b20b31525e59ba02fe2780
ammartafsol <ammar.js@tafsol.com>
2025-10-08 12:14:38 +0500

    updated table

--- src/components/Template/AnalyticsTemplate/AnalyticsTemplate.jsx
+++ src/components/Template/AnalyticsTemplate/AnalyticsTemplate.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from "./AnalyticsTemplate.module.css"
+import { Container } from 'react-bootstrap';
+import TopHeader from '@/components/atoms/TopHeader/TopHeader';
 
 const AnalyticsTemplate = () => {
   return (
-    <div>AnalyticsTemplate</div>
+    <div>
+      <Container>
+        <TopHeader title="Analytics" backButton={false} />
+      </Container>
+    </div>
   )
 }
 
