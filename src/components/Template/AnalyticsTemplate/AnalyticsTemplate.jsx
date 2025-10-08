1cd4b7b595d0c1b64e3145d938d997cec3525c30
Hamza-Tafsol <hamza.rjs.tafsol@gmail.com>
2025-10-08 17:33:48 +0500

    pro

--- src/components/Template/AnalyticsTemplate/AnalyticsTemplate.jsx
+++ src/components/Template/AnalyticsTemplate/AnalyticsTemplate.jsx
@@ -1,11 +1,18 @@
-'use client'
-import React from 'react';
-import classes from "./AnalyticsTemplate.module.css"
-import { Col, Container, Row } from 'react-bootstrap';
-import TopHeader from '@/components/atoms/TopHeader/TopHeader';
-import AnalyticsCard from '@/components/atoms/AnalyticsCard/AnalyticsCard';
-import { HiMiniUsers, HiMiniUserGroup, HiMiniCalendarDays, HiMiniCurrencyDollar } from 'react-icons/hi2';
-import GraphsFilter from '@/components/atoms/GraphsFilter/GraphsFilter';
+"use client";
+import React from "react";
+import classes from "./AnalyticsTemplate.module.css";
+import { Col, Container, Row } from "react-bootstrap";
+import TopHeader from "@/components/atoms/TopHeader/TopHeader";
+import AnalyticsCard from "@/components/atoms/AnalyticsCard/AnalyticsCard";
+import {
+  HiMiniUsers,
+  HiMiniUserGroup,
+  HiMiniCalendarDays,
+  HiMiniCurrencyDollar,
+} from "react-icons/hi2";
+import GraphsFilter from "@/components/atoms/GraphsFilter/GraphsFilter";
+import BarChart from "@/components/molecules/Chart/BarChart/BarChart";
+import ShadowWrapper from "@/components/atoms/ShadowWrapper/ShadowWrapper";
 
 const AnalyticsTemplate = () => {
   // Sample analytics data
@@ -14,26 +21,26 @@ const AnalyticsTemplate = () => {
       title: "Total Therapists",
       value: "344",
       change: "+36%",
-      icon: HiMiniUsers
+      icon: HiMiniUsers,
     },
     {
-      title: "Total Clients", 
+      title: "Total Clients",
       value: "344",
       change: "+36%",
-      icon: HiMiniUserGroup
+      icon: HiMiniUserGroup,
     },
     {
       title: "Total Appointments",
-      value: "344", 
+      value: "344",
       change: "+36%",
-      icon: HiMiniCalendarDays
+      icon: HiMiniCalendarDays,
     },
     {
       title: "Revenue",
       value: "$344",
-      change: "+36%", 
-      icon: HiMiniCurrencyDollar
-    }
+      change: "+36%",
+      icon: HiMiniCurrencyDollar,
+    },
   ];
 
   return (
@@ -45,7 +52,7 @@ const AnalyticsTemplate = () => {
             <Row>
               {analyticsData.map((data, index) => (
                 <Col key={index} lg={6} md={6} sm={12} className="mb-3">
-                  <AnalyticsCard 
+                  <AnalyticsCard
                     title={data.title}
                     value={data.value}
                     change={data.change}
@@ -56,12 +63,16 @@ const AnalyticsTemplate = () => {
             </Row>
           </Col>
           <Col lg={6} md={6} sm={12} className="mb-3">
+            <ShadowWrapper>
+              <GraphsFilter startingYear={"2022"} title={"Subscription sales breakdown"} />
+              <BarChart />
+            </ShadowWrapper>
           </Col>
         </Row>
-        <GraphsFilter startingYear={'2022'} title={'New Sign Ups'}/>
+        <GraphsFilter startingYear={"2022"} title={"New Sign Ups"} />
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default AnalyticsTemplate
+export default AnalyticsTemplate;
