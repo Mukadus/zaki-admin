cd6dd6af62b652af406919d1959e11b294bc534d
tanzeela-fe <tanzeela.fe.tafsol@gmail.com>
2025-10-09 20:34:10 +0500

    imagebox

--- src/components/molecules/ContactForm/index.jsx
+++ src/components/molecules/ContactForm/index.jsx
@@ -4,10 +4,17 @@ import clsx from "clsx";
 import Input from "@/components/atoms/Input/Input";
 import Button from "@/components/atoms/Button";
 import { HiArrowRightCircle } from "react-icons/hi2";
+import UploadImageBoxNew from "@/components/molecules/UploadImageBoxNew";
 
 export default function ContactForm({ form = {} }) {
   return (
     <div className={classes.contactForm}>
+      <UploadImageBoxNew
+        state={form.values.photo}
+        setValue={(val) => form.setFieldValue("photo", val)}
+        error={form.touched.photo && form.errors.photo}
+        label="Profile Picture"
+      />
       <Input
         label="Name"
         placeholder="Enter your name"
