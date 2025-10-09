96f5e0bdca2d3cd973a49550f4e1c252ea862494
ammartafsol <ammar.js@tafsol.com>
2025-10-09 19:48:38 +0500

    updated

--- src/app/(auth)/sign-up/page.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import SignUpTemplate from "@/components/Template/SignUpTemplate";
-export default function SignUpPage() {
-  return (
-    <div>
-      <SignUpTemplate />
-    </div>
-  );
-}
